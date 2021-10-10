910df164b777a5ea1ab4adef605b5900ea1ed2b7
Saad Tariq <saad.tarik@outlook.com>
2021-10-10 14:25:54 +0100

    difference in final day value

--- javascript/src/PortfolioPerformance.js
+++ javascript/src/PortfolioPerformance.js
@@ -1,5 +1,3 @@
-// const moment = require( "moment");
-// const collect = require("collect.js")
 
 const prices = [
     { effectiveDate: new Date(2021, 8, 1, 5, 0, 0), price: 35464.53 },
@@ -18,131 +16,57 @@ const transactions = [
     { effectiveDate: new Date(2021, 8, 7, 9, 0, 0), value: 0.1 },
 ];
 
+export function getDailyPortfolioValues() {
+    const dailyValues = [];
+    //const dateValues = {};
+    let portfolioTotal = 0;
+    let coinTotal = 0;
+    let currentPrice = 0;
 
+    // Get date range and change to ISO string
+    for(let date = 1; date < 8; date++){
+        const dateRange = {};
+        dateRange["effectiveDate"] = new Date(2021, 8, date, 23, 59).toISOString().slice(0,10)
+        dateRange["value"] = portfolioTotal;
+        dailyValues.push(dateRange);
+    }
+    // Filter transaction and prices on given dates
+    for(let i = 0; i < 7; i++){
+        let indexDate = dailyValues[i]["effectiveDate"];
+        console.log("The date is " + indexDate);
+        const trans = transactions.filter(item => item.effectiveDate.toISOString().slice(0,10) === indexDate);
+        const price = prices.filter(item => item.effectiveDate.toISOString().slice(0,10) === indexDate);
+        if(price.length !== 0){
+            currentPrice = price[price.length - 1].price;
+        }
+        // update portfolio total and coin total on each iteration
+        if(trans.length >= 1){
+            for(let i = 0; i < trans.length; i++){
+                portfolioTotal +=  parseFloat((trans[i].value * currentPrice).toFixed(5))
+                // console.log("portfolio total is " + portfolioTotal);
+                coinTotal += parseFloat((trans[i].value).toFixed(4));
+                // console.log("total coin is " + coinTotal);
+            }
+        } else {
+            console.log("No transactions");
+            portfolioTotal = parseFloat((coinTotal * currentPrice).toFixed(5));
+        }
+        dailyValues[i]["value"] = portfolioTotal
 
-function getDates (startDate, endDate) {
-    const dates = []
-    let currentDate = startDate
-    const addDays = function (days) {
-        const date = new Date(this.valueOf())
-        date.setDate(date.getDate() + days)
-        return date
+        console.log("current price is " + currentPrice)
+        console.log("total coin is " + coinTotal);
+        console.log("Portfolio value is " + portfolioTotal);
     }
-    while (currentDate < endDate) {
-        dates.push(currentDate)
-        currentDate = addDays.call(currentDate, 1)
-    }
-    return dates
+    return dailyValues;
 }
-const dates = getDates(new Date(2021, 8, 1), new Date(2021, 8, 9))
-
-var startDate = new Date (2021, 8, 1, 0, 59)
-var endDate = new Date(2021, 8, 8,0, 59)
-const portfolioHoldings = prices.filter(function (trans){
-    return trans.effectiveDate >= startDate && trans.effectiveDate <= endDate
-})
-console.log(portfolioHoldings)
+console.log(getDailyPortfolioValues())
 
 
 
 
-// console.log(getDates(new Date(2021, 8, 1), new Date(2021, 8, 8)))
-//
-// Usage
-// dates.forEach(function (date) {
-//     console.log(date.toISOString().slice(0,10))
-// })
-
-// const reportingPeriods = []
-// const dateFormat = "YYYY-MM-DD"
-// const timeFormat = 'HH:mm:ss.SSS'
-// const dateTimeFormat = dateFormat + 'T' + timeFormat + 'Z'
-// function getTimestops(start, end, interval = 1, unit = 'day') {
-//     let startDateTime = moment(start, dateFormat);
-//     let endDateTime = moment(end, dateFormat);
-//     if (endDateTime.isBefore(startDateTime)) {
-//         endDateTime.add(1, 'day');
-//     }
-//     let timeStops = [];
-//     while (startDateTime.isBefore(endDateTime)) {
-//         timeStops.push(new moment(startDateTime).format(dateFormat));
-//         startDateTime.add(interval, unit);
-//     }
-//     timeStops.push(endDateTime.format(dateFormat))
-//
-//     // if (timeStops.length % 2 !== 0) {
-//     //     timeStops.pop()
-//     //     timeStops.push(timeStops[timeStops.length - 1])
-//     //     timeStops.push(endDateTime.format(da))
-//     // }
-//     return timeStops;
-// }
-//
-// const timeStops = getTimestops("2021-09-01", "2021-09-07");
-// console.log(timeStops)
-//
-// var getDailyPortfolioValueReport = timeStops.map(item => {
-//     return {effectiveDate: item, value: 0}
-// })
-// // console.log(getDailyPortfolioValueReport)
-//
-// let newPrices =collect(prices).groupBy((item)=>{
-//     return item.effectiveDate.toISOString().slice(0,10)
-// })
-
-// console.log('prices: ',newPrices)
-
-// newPrices.each((item)=>{
-//     console.log(item)
-// })
-
-// let newTransactions = collect(transactions).groupBy((item)=>{
-//     return item.effectiveDate.toISOString().slice(0,10)
-// })
-// let newTransactionss = newTransactions.map((item)=>{
-//     console.log(item)
-//     return {item:item.sum('value')}
-// })
-// console.log('newTransactions: ',newTransactionss)
-// var getDailyPortFolioValue = transactions.map((item)=>{
-//     let effectiveDate = item.effectiveDate.toISOString().slice(0,10)
-//     let date = newPrices.get(effectiveDate)
-
-    // console.log(effectiveDate,maxDatePrice)
-// })
-
-// function getDailyPortfolioValues() {
-//     let dailyPortfolioValue = []
-//     let currentCoinPrice = 0;
-//     let portfolioHoldings = 0;
-//     let dateRange = []
-//
-//     for(let day = 2; day <= 8; day++ ) {
-//         let dates = new Date(2021, 8, day).toISOString().slice(0,10)
-//         dateRange.push(dates)
-//     }
-//
-//
-//
-//
-//     return dateRange
-// }
-// console.log(getDailyPortfolioValues())
 
 
-// console.log(reportingPeriods)
-// export function getDailyPortfolioValues()  {
-//     const dailyPortfolioValues = [];
-//     dates.forEach((date) => {
-//         dailyPortfolioValues.push({"effectiveDate": date.toISOString().slice(0,10), "value": 0 });
-//     })
-//     // transactions.forEach((transaction) => {
-//     //     dailyPortfolioValues.push({"effectiveDate": transaction.effectiveDate.sort(function (a, b) {
-//     //         return new Date(a.effectiveDate) - new Date(b.effectiveDate)
-//     //         })
-//     // })
-//     return dailyPortfolioValues
-// }
+
 
 //Nucleus has a requirement to report on the portfolio performance of our clients’ bitcoin investments.
 // Skeleton implementations have been provided in Java and JavaScript.
