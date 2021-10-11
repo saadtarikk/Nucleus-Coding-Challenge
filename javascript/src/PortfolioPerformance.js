c3314bdaa74a6d8f631660b03ef53d416f060e6b
Saad Tariq <saad.tarik@outlook.com>
2021-10-11 12:30:37 +0100

    Final day value is different, not able to figure out the issue yet

--- javascript/src/PortfolioPerformance.js
+++ javascript/src/PortfolioPerformance.js
@@ -42,39 +42,20 @@ export function getDailyPortfolioValues() {
         // update portfolio total and coin total on each iteration
         if(trans.length >= 1){
             for(let i = 0; i < trans.length; i++){
-                portfolioTotal +=  parseFloat((trans[i].value * currentPrice).toFixed(5))
-                // console.log("portfolio total is " + portfolioTotal);
-                coinTotal += parseFloat((trans[i].value).toFixed(4));
-                // console.log("total coin is " + coinTotal);
+                portfolioTotal +=  trans[i].value * currentPrice
+                console.log("portfolio total is " + portfolioTotal);
+                coinTotal += trans[i].value
+                console.log("total coin is " + coinTotal);
             }
         } else {
             console.log("No transactions");
-            portfolioTotal = parseFloat((coinTotal * currentPrice).toFixed(5));
+            portfolioTotal = coinTotal * currentPrice
         }
-        dailyValues[i]["value"] = portfolioTotal
+        dailyValues[i]["value"] = parseFloat((portfolioTotal).toFixed(5))
 
-        console.log("current price is " + currentPrice)
-        console.log("total coin is " + coinTotal);
-        console.log("Portfolio value is " + portfolioTotal);
+        // console.log("current price is " + currentPrice)
+        // console.log("total coin is " + coinTotal);
+        // console.log("Portfolio value is " + portfolioTotal);
     }
     return dailyValues;
 }
-console.log(getDailyPortfolioValues())
-
-
-
-
-
-
-
-
-//Nucleus has a requirement to report on the portfolio performance of our clients’ bitcoin investments.
-// Skeleton implementations have been provided in Java and JavaScript.
-// Choose one of the two languages, implement getDailyPortfolioValues and get the unit test passing.
-// In the PortfolioPerformance file, a list of bitcoin prices and a list of transactions are provided.
-// Both lists are sorted by date in ascending order.
-// At the beginning of the reporting period, assume that the client has 0 bitcoins.
-// The reporting period is 01-09-2021 to 07-09-2021.
-// For each day in the reporting period, output the date and the portfolio value.
-// This should be output in ascending order by date.
-// Portfolio value = number of units held at the end of the given day * most recent fund price for the given day.
